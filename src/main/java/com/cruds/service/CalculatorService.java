c02c5b31da7957bf0dc922c8011a21501c711020
sahil jain <62873731+sahilj713@users.noreply.github.com>
2023-06-27 17:47:10 +0530

    Update CalculatorService.java

--- src/main/java/com/cruds/service/CalculatorService.java
+++ src/main/java/com/cruds/service/CalculatorService.java
@@ -13,3 +13,33 @@ public class CalculatorService {
 	}
 
 }
+package com.cruds.service;
+
+public class CalculatorService {
+	
+	public static int add(int x, int y)
+	{
+		return x + y;
+	}
+
+	public static int subtract(int x, int y)
+	{
+		return x - y;
+	}
+
+}
+package com.cruds.service;
+
+public class CalculatorService {
+	
+	public static int add(int x, int y)
+	{
+		return x + y;
+	}
+
+	public static int subtract(int x, int y)
+	{
+		return x - y;
+	}
+
+}
